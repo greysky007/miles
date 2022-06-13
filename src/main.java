4a9c62b3a44fad319b900e878a82b3aba2b12bd1
greysky <antonysnork@gmail.com>
2022-06-13 13:03:00 +0300

    Reformating code

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int amount = 101;
         int mile = 20;
 
-        int result =amount/mile;
+        int result = amount / mile;
         System.out.println("Начислено миль: " + result);
     }
 }
